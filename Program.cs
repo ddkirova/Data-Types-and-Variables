7b8ac0ca98c4de5c0aa1b8f3ca10bf837e1158d0
dd_kirova <dd_kirova@students.softuni.bg>
2018-03-11 09:49:28 +0200

    Add Data-Types-and-Variables cs file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _09_Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= input; i++)
+            {
+                int sumOfDigits = 0;
+                int currentNumber = i;
+                while (currentNumber > 0)
+                {
+                    sumOfDigits += currentNumber % 10;
+                    currentNumber = currentNumber / 10;
+                }
+                bool isTrue = (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11);
+                Console.WriteLine($"{i} -> {isTrue}");
+            }
+        }
+    }
+}
